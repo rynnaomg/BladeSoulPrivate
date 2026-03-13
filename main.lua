3ffe3b020b7cece8329ab3b16f5cde55ac86b28e
rynnaomg <fedorivdanil2000@gmail.com>
2026-03-13 19:21:04 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,9 +3,9 @@
 -- Version: 1.1
 
 -- Load required modules
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/Library.lua"))()
-local GameCheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/GameCheck.lua"))()
-local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/Config.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/Library.lua?nocache=" .. tostring(os.time())))()
+local GameCheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/GameCheck.lua?nocache=" .. tostring(os.time())))()
+local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/rynnaomg/BladeSoulPrivate/main/Config.lua?nocache=" .. tostring(os.time())))()
 
 -- Check if we're in the correct game
 if GameCheck:IsCorrectGame() then
